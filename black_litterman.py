820fec41884c9708fa68626b0b73169832a45fcd
sps0094 <sarathy.srgm@gmail.com>
2020-06-18 15:34:07 +0530

    start black litterman

--- black_litterman.py
+++ black_litterman.py
@@ -4,6 +4,7 @@ import pandas as pd
 from pandas.tseries.offsets import MonthBegin
 import plotly.graph_objs as go
 import plotly.offline as pyo
+
 pyo.init_notebook_mode(connected=True)
 from jupyter_dash import JupyterDash
 import dash
@@ -22,20 +23,56 @@ checkoptions = [
     {'label': 'Views', 'value': 4},
     {'label': 'Pick_Mat', 'value': 5},
 ]
-app.layout = html.Div([html.Div([html.Label(children='Enter list of asset names: '),
-                                dcc.Input(id='asset_list', value='', type='text')], style={'display': 'inline-block'}),
-                                html.Div([html.Label(children='Enter no of views: '),
-                                dcc.Input(id='no_views', value='', type='number')], style={'display': 'inline-block'}),
-                                html.Div([dcc.Checklist(id='display_table', options=checkoptions, value=[], labelStyle={'display': 'inline-block'})]),
-                                html.Div(id='wts_table_container', children=[dt.DataTable(id='wts_table', editable=True)], style={'display': 'block'}),
-                                html.Div(id='rhos_table_container', children=[dt.DataTable(id='rhos_table', editable=True)], style={'display': 'block'}),
-                                html.Div(id='vol_table_container', children=[dt.DataTable(id='vol_table', editable=True)], style={'display': 'block'}),
-                                html.Div(id='cov_table_container', children=[dt.DataTable(id='cov_table', editable=True)], style={'display': 'block'}),
-                                html.Div(id='view_table_container', children=[dt.DataTable(id='view_table', editable=True)], style={'display': 'block'}),
-                                html.Div(id='pick_table_container', children=[dt.DataTable(id='pick_table', editable=True)], style={'display': 'block'}),
-                                html.Div(id='dump', style={'display': 'block'}),
-                                html.Button(id='submit', children='SUBMIT', n_clicks=0, style={'display': 'inline-block'}),
-                                html.Button(id='update', children='UPDATE', n_clicks=0, style={'display': 'none'})])
+app.layout = html.Div(
+    [html.Div([html.Div([dcc.Checklist(id='local_file', options=[{'label': 'Y', 'value': 1}], value=[],
+                                       labelStyle={'display': 'inline-block'})]),
+               html.Label(children='Enter list of asset names: '),
+               dcc.Input(id='asset_list', value='', type='text')], style={'display': 'inline-block'}),
+     html.Div([html.Label(children='Enter no of views: '),
+               dcc.Input(id='no_views', value='', type='number')], style={'display': 'inline-block'}),
+     html.Div([dcc.Checklist(id='display_table', options=checkoptions, value=[],
+                             labelStyle={'display': 'inline-block'})]),
+     html.Div(id='wts_table_container', children=[dt.DataTable(id='wts_table', editable=True)],
+              style={'display': 'block'}),
+     html.Div(id='rhos_table_container', children=[dt.DataTable(id='rhos_table', editable=True)],
+              style={'display': 'block'}),
+     html.Div(id='vol_table_container', children=[dt.DataTable(id='vol_table', editable=True)],
+              style={'display': 'block'}),
+     html.Div(id='cov_table_container', children=[dt.DataTable(id='cov_table', editable=True)],
+              style={'display': 'block'}),
+     html.Div(id='view_table_container', children=[dt.DataTable(id='view_table', editable=True)],
+              style={'display': 'block'}),
+     html.Div(id='pick_table_container', children=[dt.DataTable(id='pick_table', editable=True)],
+              style={'display': 'block'}),
+     html.Div(id='dump', style={'display': 'none'}),
+     html.Div(id='dump_no', style={'display': 'none'}),
+     html.Button(id='submit', children='SUBMIT', n_clicks=0, style={'display': 'inline-block'}),
+     html.Button(id='update', children='UPDATE', n_clicks=0, style={'display': 'none'})])
+
+
+@app.callback([Output('asset_list', 'value'),
+               Output('dump_no', 'children')],
+              [Input('local_file', 'value')])
+def upd_asset_list(localfile):
+    if localfile:
+        ind_vw_2014 = erk.get_df('data/ind49_m_vw_rets.csv', to_per=True, start_period='2014')
+        ind_mkt_wts_2014 = erk.plot_corr_mktret(ind_ret_filename='data/ind49_m_vw_rets.csv',
+                                                n_firms_filename='data/ind49_m_nfirms.csv',
+                                                size_filename='data/ind49_m_size.csv',
+                                                start_period='2014',
+                                                end_period=None,
+                                                to_per=True,
+                                                retrieve_mkt_cap_wts=True,
+                                                format='%Y%m')
+        asset_list = [' '.join(list(ind_vw_2014.columns))]
+        return asset_list, len(ind_vw_2014.columns)
+
+
+@app.callback([Output('no_views', 'max')],
+              [Input('dump_no', 'children')])
+def max_views(max):
+    return [int(max)]
+
 @app.callback([Output('wts_table_container', 'style'),
                Output('rhos_table_container', 'style'),
                Output('vol_table_container', 'style'),
@@ -60,6 +97,8 @@ app.layout = html.Div([html.Div([html.Label(children='Enter list of asset names:
                State('asset_list', 'value'),
                State('no_views', 'value')])
 def upd_visibility(n_clicks, value, asset_list, no_views):
+    if isinstance(asset_list, list):
+        asset_list = asset_list[0]
     asset_list = list(asset_list.split(' '))
     disp_wts = 'block' if 0 in value else 'none'
     disp_rhos = 'block' if 1 in value else 'none'
@@ -67,20 +106,23 @@ def upd_visibility(n_clicks, value, asset_list, no_views):
     disp_cov = 'block' if 3 in value else 'none'
     disp_view = 'block' if 4 in value else 'none'
     disp_pick = 'block' if 5 in value else 'none'
-    col_wts = [{'id':'asset_name_wts', 'name':'Assets'}] + [{'id':'wts', 'name':'Prior_weights'}]
+    col_wts = [{'id': 'asset_name_wts', 'name': 'Assets'}] + [{'id': 'wts', 'name': 'Prior_weights'}]
     data_wts = [dict(asset_name_wts=asset) for asset in asset_list]
     col_vol = [{'id': 'asset_name_wts', 'name': 'Assets'}] + [{'id': 'vol', 'name': 'Prior_volatility'}]
     data_vol = [dict(asset_name_wts=asset) for asset in asset_list]
     col_rhos = [{'id': 'rhos', 'name': 'Rhos'}] + [{'id': asset, 'name': asset} for asset in asset_list]
     data_rhos = [dict(rhos=asset) for asset in asset_list]
-    col_views = [{'id':'views_no', 'name':'Assets'}] + [{'id':'views', 'name':'views'}]
-    data_views = [dict(views_no=k) for k in range(1, no_views+1)]
-    col_cov = [{'id':'cov', 'name':'cov_mat'}] + [{'id':asset, 'name':asset} for asset in asset_list]
+    col_views = [{'id': 'views_no', 'name': 'Assets'}] + [{'id': 'views', 'name': 'views'}]
+    data_views = [dict(views_no=k) for k in range(1, no_views + 1)]
+    col_cov = [{'id': 'cov', 'name': 'cov_mat'}] + [{'id': asset, 'name': asset} for asset in asset_list]
     data_cov = [dict(cov=asset) for asset in asset_list]
-    col_pick = [{'id':'pick', 'name':'pick_mat'}] + [{'id':asset, 'name':asset} for asset in asset_list]
-    data_pick = [dict(pick=k) for k in range(1, no_views+1)]
-    return {'display':disp_wts}, {'display':disp_rhos}, {'display':disp_vol},{'display':disp_cov}, {'display':disp_view}, {'display':disp_pick}, \
-           col_wts, data_wts, col_rhos, data_rhos, col_vol, data_vol, col_cov, data_cov, col_views, data_views, col_pick, data_pick,{'display':'inline-block'}
+    col_pick = [{'id': 'pick', 'name': 'pick_mat'}] + [{'id': asset, 'name': asset} for asset in asset_list]
+    data_pick = [dict(pick=k) for k in range(1, no_views + 1)]
+    return {'display': disp_wts}, {'display': disp_rhos}, {'display': disp_vol}, {'display': disp_cov}, {
+        'display': disp_view}, {'display': disp_pick}, \
+           col_wts, data_wts, col_rhos, data_rhos, col_vol, data_vol, col_cov, data_cov, col_views, data_views, col_pick, data_pick, {
+               'display': 'inline-block'}
+
 
 @app.callback([Output('dump', 'children')],
               [Input('update', 'n_clicks')],
@@ -116,5 +158,6 @@ def update_values(n_clicks, wts, cov, rhos, vol, views, pick_mat, asset_list, va
     if 5 in value:
         pick_df = pd.DataFrame(pick_mat, columns=[asset for asset in asset_list], index=views.index).astype('float')
     return ' '
+
 
 app.run_server()
